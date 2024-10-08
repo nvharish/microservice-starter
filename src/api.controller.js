18831c1df8a247d6c0912ef11a577f1c7cf2da8d
N V Harish <nv.harish@outlook.com>
2024-10-08 21:36:06 +0530

    token validation patch

--- src/api.controller.js
+++ src/api.controller.js
@@ -6,7 +6,7 @@ module.exports = function handler(app) {
      * you can remove the following code if JWT verification is not required for your service
      */
     app.use((req, res, next) => {
-        const jwt = req.get('Authorization').replace(/Bearer \s*/, '');
+        const jwt = req.get('Authorization').replace('Bearer ', '');
 
         verifyJwt(jwt, (err, user) => {
             if (err) {
